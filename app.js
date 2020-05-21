393aa21e68afbeed6167ff9c982e936ca2e2c11f
Amelia Waggoner <wildginger@wavecable.com>
2020-05-21 12:08:18 -0700

    new file

---
+++ app.js
@@ -0,0 +1,39 @@
+'use strict'
+
+function askName() {
+   var userName = prompt ('What is your name?');
+   console.log (userName);
+   return userName;
+}
+
+function askdogBreed(dog){
+    var dogBreed = prompt (dog +'What kind of dog breed do you have?').toLowerCase();
+    console.log(dogBreed);
+    return dogBreed;
+}
+
+function typeOfDog() {
+    var typeOfDog = prompt ('What type of dog do you have?');
+    console.log(typeOfDog);
+}
+
+function likedogBreed(dogBreedFromFunction){
+    if (dogBreedFromFunction === 'golden retriever'){
+        alert ('Me too!'); 
+    } else if (dogBreedFromFunction === 'shar pei') {
+        alert ('Shar Pei is a cute breed of dog too!');
+    } else {
+        alert ('Both the shar pei and golden retriever are good breeds!')
+    }
+}
+
+
+// if (typeOfDog === 'chihuahua'){
+    //     alert('The chihuahua dog breed is tempermental!');
+    // }
+
+    var userNameFromFunction = askName();
+    var dogBreedFromFunction = askdogBreed (userNameFromFunction);
+    var typeOfDogFromFunction = typeOfDog ();
+    likedogBreed(dogBreedFromFunction)
+    
--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ var userName = prompt ('What is your name?');
 console.log (name);
 var greeting;
 
-if (userName > Amelia) {
+if (userName = 'Amelia') {
     greeting = 'Hello Amelia!';
 } else {
     greeting = 'Welcome!';
@@ -13,23 +13,21 @@ if (userName > Amelia) {
 
 var today = new Date ();
 var hourNow = today.getHours();
-var greeting;
-
+ 
 if (hourNow > 8) {
     greeting = 'Good Morning!';
 } else if (hourNow > 12) {
     greeting = 'Good Afternoon!';
 } else if (hourNow > 6) {
     greeting = 'Good Evening!';
+}
+var dogBreed = prompt ('What breed is your dog?');
+console.log(dogBreed)
 
-var dogBreed = prompt ('What breed is your dog?);
-console.log (breed)
-var greeting;
-
-if (dogBreed > Golden); {
+if (dogBreed = 'Golden') {
     greeting = 'Retriever'
 } else {
-    greeting = 'Bichon'
+    greeting = 'Shar Pei'
 }
 
-document.write('<title>' + userName + greeting + dogBreed '<title>');
+document.write('<h3>' + userName + greeting + dogBreed + '<h3>');
